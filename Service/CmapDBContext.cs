f2b0b63d41e02c2c8fb7bbd138d2f308dfe6fd34
fresnobob-71 <137520526+fresnobob-71@users.noreply.github.com>
2025-03-19 04:28:05 +0000

    Add project files.

---
+++ Service/CmapDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class CmapDBContext : DbContext
+    {
+        public CmapDBContext(DbContextOptions<CmapDBContext> options): base(options) { }
+
+        public DbSet<Timesheet> Timesheets { get; set; }
+    }
+}
---
+++ Service/Models/Timesheet.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.Models
+{
+    public class Timesheet
+    {
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+        public string Project { get; set; }
+        public string Description { get; set; }
+        public double HoursWorked { get; set; }
+    }
+}
